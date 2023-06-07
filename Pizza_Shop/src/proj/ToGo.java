c7c08ee51f52bbc20f26cfd642e6ca6bae9732ec
SuperMoathx7 <109073912+SuperMoathx7@users.noreply.github.com>
2023-06-07 20:56:13 +0300

    Add files via upload

---
+++ Pizza_Shop/src/proj/ToGo.java
@@ -0,0 +1,14 @@
+package proj;
+
+
+
+//Moath Wajeeh Abdalbaqi
+//1210125
+//S11
+public class ToGo extends PizzaOrder{
+ public ToGo() {}
+ public ToGo(String customerName,int size,int numberOfToppings,double toppingPrice) {
+	  super(customerName,size, numberOfToppings, toppingPrice); 
+ }
+ // Nothing to say here
+}
